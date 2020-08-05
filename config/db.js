10e88bcfa6c5e945f28f8ef453f703a80ba42ec1
ruzny <ruzny@ruzny.com>
2020-08-05 19:35:24 +0530

    fixed some issues on connecting to mongo

--- config/db.js
+++ config/db.js
@@ -1,16 +1,20 @@
-import mongoose from 'mongoose';
-// import server from '../server';
+const mongoose = require('mongoose');
+// const server = require('../server');
 
-export default async () => {
+module.exports = async (server) => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
     if (conn) {
       console.log('mongo connection successful..');
-      // server.listen(process.env.PORT || 5000, () =>
-      //   console.log(
-      //     `server running on ${process.env.NODE_ENV} mode, port ${process.env.PORT}..`
-      //   )
-      // );
+      server.listen(process.env.PORT || 5000, () =>
+        console.log(
+          `server running on ${process.env.NODE_ENV} mode, port ${process.env.PORT}..`
+        )
+      );
     }
   } catch (error) {
     console.log('mongo connection failed..');
--- server.js
+++ server.js
@@ -1,24 +1,18 @@
-import express from 'express';
-import { createServer } from 'http';
-import dotenv from 'dotenv';
-import logger from 'morgan';
+const express = require('express');
+const { createServer } = require('http');
+const dotenv = require('dotenv');
+const logger = require('morgan');
 
 // DB CONNECTTION
-import connectDB from './config/db';
+const db = require('./config/db');
 
 const app = express();
 const server = createServer(app);
 
 dotenv.config({ path: './config/config.env' });
 
-connectDB();
+db(server);
 
 if (process.env.NODE_ENV === 'development') app.use(logger('dev'));
 
-server.listen(process.env.PORT || 5000, () =>
-  console.log(
-    `server running on ${process.env.NODE_ENV} mode, port ${process.env.PORT}..`
-  )
-);
-
-export default server;
+module.exports = server;
