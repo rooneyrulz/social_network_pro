7583e2e17767ed4105f1fcac932c1bf7442a8bd1
ruzny <ruzny@ruzny.com>
2020-08-07 19:59:39 +0530

    working on post api | update post

--- routes/post.js
+++ routes/post.js
@@ -35,6 +35,25 @@ router.get('/:post_id', isAuth, async (req, res, next) => {
   }
 });
 
+router.put('/:post_id', isAuth, async (req, res, next) => {
+  const { post_id } = req.params;
+  const { text } = req.body;
+  try {
+    const post = await Post.findById(post_id).lean();
+    if (!post) return res.status(404).send('Post not found..');
+    if (post.owner.toString() !== req.user._id.toString())
+      return res.status(400).send('Permission denied..');
+    const isUpdated = await Post.findByIdAndUpdate(
+      { _id: post_id },
+      { text, owner: req.user },
+      { new: true }
+    );
+    return res.status(200).json(isUpdated);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+
 router.delete('/:post_id', isAuth, async (req, res, next) => {
   const { post_id } = req.params;
   const { _id } = req.user;
