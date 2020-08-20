39196b0b5370c5e3fdd4499529d76d6ce4967a95
ruzny <ruzny@ruzny.com>
2020-08-20 23:22:36 +0530

    simple registration form setup | added some styles

--- client-mobile/screens/auth/Register.js
+++ client-mobile/screens/auth/Register.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 import ScreenContainer from '../../screens/ScreenContainer';
