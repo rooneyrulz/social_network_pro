b35f81fddae5f72fcd1f76532b45ace6915e2b51
ruzny <ruzny@ruzny.com>
2020-08-20 07:21:24 +0530

    setup formik | login form

--- client-mobile/screens/auth/Login.js
+++ client-mobile/screens/auth/Login.js
@@ -1,55 +1,14 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Text, Button, TextInput, Alert } from 'react-native';
-
-// Redux
-import { connect } from 'react-redux';
-import { loginUser } from '../../actions/auth';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
 
 import ScreenContainer from '../../screens/ScreenContainer';
+import LoginForm from '../../forms/LoginForm';
 
-const Login = ({ navigation, auth: { isAuthenticated }, loginUser }) => {
-  const [formData, setFormData] = useState({
-    username: '',
-    password: '',
-  });
-
-  const { username, password } = formData;
-
-  const onHandleSubmit = (e) => {
-    if (!username || !password) Alert.alert('Oops! Invalid fields');
-    loginUser(formData);
-  };
-
-  if (isAuthenticated) navigation.navigate('Home');
-
+const Login = ({ navigation }) => {
   return (
     <ScreenContainer>
       <View style={styles.loginContainer}>
-        <View style={styles.inputFieldWrapper}>
-          <TextInput
-            style={styles.inputField}
-            placeholder='Username'
-            onChangeText={(value) =>
-              setFormData({ ...formData, username: value })
-            }
-          />
-        </View>
-        <View style={styles.inputFieldWrapper}>
-          <TextInput
-            style={styles.inputField}
-            placeholder='Password'
-            onChangeText={(value) =>
-              setFormData({ ...formData, password: value })
-            }
-          />
-        </View>
-        <View>
-          <Button
-            style={styles.loginBtn}
-            title='Login'
-            onPress={(e) => onHandleSubmit(e)}
-          />
-        </View>
+        <LoginForm />
       </View>
     </ScreenContainer>
   );
@@ -60,20 +19,6 @@ const styles = StyleSheet.create({
     width: '90%',
     marginHorizontal: '5%',
   },
-
-  inputFieldWrapper: {
-    marginBottom: 10,
-  },
-
-  inputField: {
-    backgroundColor: '#fff',
-    paddingVertical: 10,
-    paddingHorizontal: 5,
-  },
 });
 
-const mapStateToProps = (state) => ({
-  auth: state.auth,
-});
-
-export default connect(mapStateToProps, { loginUser })(Login);
+export default Login;
